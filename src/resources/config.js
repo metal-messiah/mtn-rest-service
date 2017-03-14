62eb951e9c730db141879b897f0b9882a4294422
Allen Sudweeks <allensudweeks@gmail.com>
2017-03-14 16:12:33 -0600

    Remove remnants of basic auth implementation

--- src/resources/config.js
+++ src/resources/config.js
@@ -16,13 +16,7 @@ var config = {
     },
     server: {
         port: 3000
-    },
-    users: [
-        {
-            username: 'mtn_rest_service',
-            password: 'R3t@1lAdv1s0rs'
-        }
-    ]
+    }
 };
 
 module.exports = config;

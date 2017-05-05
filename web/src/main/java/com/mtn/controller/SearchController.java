30800f880645685945a156aeddc8af1259009d8a
Allen Sudweeks <allensudweeks@gmail.com>
2017-05-04 21:20:36 -0600

    Add some simple search request validation

--- web/src/main/java/com/mtn/controller/SearchController.java
+++ web/src/main/java/com/mtn/controller/SearchController.java
@@ -1,8 +1,10 @@
 package com.mtn.controller;
 
+import com.mtn.constant.SearchType;
 import com.mtn.model.view.search.SearchRequest;
 import com.mtn.model.view.search.SearchResultView;
 import com.mtn.service.SearchService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,7 +26,17 @@ public class SearchController {
 
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity search(@RequestBody SearchRequest request) {
+        validateSearchRequest(request);
+
         List<SearchResultView> searchResults = searchService.search(request);
         return ResponseEntity.ok(searchResults);
+    }
+
+    private void validateSearchRequest(SearchRequest searchRequest) {
+        if (searchRequest.getSearchType() == null) {
+            throw new IllegalArgumentException(String.format("searchType must be one of: %s", StringUtils.join(SearchType.values(), ", ")));
+        } else if (searchRequest.getDistance() != null && searchRequest.getDistance() <= 0) {
+            throw new IllegalArgumentException("distance must be greater than 0");
+        }
     }
 }

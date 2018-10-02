aa7d31f48f1187e460bfd2bc8faaa51470e97d06
Tyler Tippetts <tyjoti89@gmail.com>
2018-10-02 12:39:46 -0600

    Fixed bad migration script

--- core/src/main/resources/db/migration/V4.0.0.2__updated_pharmacy_extraction_fields.sql
+++ core/src/main/resources/db/migration/V4.0.0.2__updated_pharmacy_extraction_fields.sql
@@ -1,4 +1,4 @@
-UPDATE `mtn_dev`.`extraction_field` SET `table`='StoreSurvey', `field_mapping`='storeSurvey.pharmacyScriptsWeekly' WHERE  `extraction_field_id`=12;
-UPDATE `mtn_dev`.`extraction_field` SET `table`='StoreSurvey', `field_mapping`='storeSurvey.pharmacyAvgDollarsPerScript' WHERE  `extraction_field_id`=13;
-UPDATE `mtn_dev`.`extraction_field` SET `table`='StoreSurvey', `field_mapping`='storeSurvey.pharmacyPharmacistCount' WHERE  `extraction_field_id`=14;
-UPDATE `mtn_dev`.`extraction_field` SET `table`='StoreSurvey', `field_mapping`='storeSurvey.pharmacyTechnicianCount' WHERE  `extraction_field_id`=15;
+UPDATE `extraction_field` SET `table`='StoreSurvey', `field_mapping`='storeSurvey.pharmacyScriptsWeekly' WHERE  `extraction_field_id`=12;
+UPDATE `extraction_field` SET `table`='StoreSurvey', `field_mapping`='storeSurvey.pharmacyAvgDollarsPerScript' WHERE  `extraction_field_id`=13;
+UPDATE `extraction_field` SET `table`='StoreSurvey', `field_mapping`='storeSurvey.pharmacyPharmacistCount' WHERE  `extraction_field_id`=14;
+UPDATE `extraction_field` SET `table`='StoreSurvey', `field_mapping`='storeSurvey.pharmacyTechnicianCount' WHERE  `extraction_field_id`=15;

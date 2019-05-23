b12a5fb0a687b9022fc7c1015f4f752be59d0681
Jordan Porter <jordan@mtnra.com>
2019-05-23 16:40:15 -0600

    MDS-496 added properties to SimpleBannerView to allow for company info in banner dialog

--- core/src/main/java/com/mtn/model/simpleView/SimpleBannerView.java
+++ core/src/main/java/com/mtn/model/simpleView/SimpleBannerView.java
@@ -2,12 +2,15 @@ package com.mtn.model.simpleView;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.mtn.model.domain.Banner;
+import com.mtn.model.domain.Company;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SimpleBannerView extends SimpleAuditingEntityView {
 
     private String bannerName;
     private String logoFileName;
+    private String companyName;
+    private String parentCompanyName;
 
     public SimpleBannerView() {
     }
@@ -16,6 +19,18 @@ public class SimpleBannerView extends SimpleAuditingEntityView {
         super(banner);
         this.bannerName = banner.getBannerName();
         this.logoFileName = banner.getLogoFileName();
+        Company company = banner.getCompany();
+        this.companyName = company.getCompanyName();
+        this.getHighestParentCompanyName(company);
+    }
+
+    private void getHighestParentCompanyName(Company company) {
+        Company parentCompany = company.getParentCompany();
+        if (parentCompany == null) {
+            this.parentCompanyName = company.getCompanyName();
+        } else {
+            this.getHighestParentCompanyName(parentCompany);
+        }
     }
 
     public String getBannerName() {
@@ -32,5 +47,21 @@ public class SimpleBannerView extends SimpleAuditingEntityView {
 
     public void setLogoFileName(String logoFileName) {
         this.logoFileName = logoFileName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getParentCompanyName() {
+        return parentCompanyName;
+    }
+
+    public void setParentCompanyName(String parentCompanyName) {
+        this.parentCompanyName = parentCompanyName;
     }
 }

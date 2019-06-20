8bb06c6a0fd2a2db56bf37dec42dcbe7dc2db5ce
Tyler Tippetts <tyjoti89@gmail.com>
2019-06-20 11:57:20 -0600

    Support for store list detail data

--- core/src/main/java/com/mtn/model/simpleView/SiteMarker.java
+++ core/src/main/java/com/mtn/model/simpleView/SiteMarker.java
@@ -1,6 +1,7 @@
 package com.mtn.model.simpleView;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.mtn.constant.StoreType;
 import com.mtn.model.domain.Site;
 import com.mtn.model.domain.UserProfile;
 
@@ -20,6 +21,14 @@ public class SiteMarker {
 	private Boolean backfilledNonGrocery;
 	private LocalDateTime updatedDate;
 	private List<StoreMarker> stores;
+	private boolean vacant;
+	private String address;
+	private String city;
+	private String state;
+	private String postalCode;
+	private String quad;
+	private String intersectionStreetPrimary;
+	private String intersectionStreetSecondary;
 
 	public SiteMarker(Site site) {
 		this.id = site.getId();
@@ -43,6 +52,16 @@ public class SiteMarker {
 
 		this.stores = site.getStores().stream().filter(store -> store.getDeletedDate() == null).map(StoreMarker::new)
 				.collect(Collectors.toList());
+
+		this.vacant = this.stores.stream().noneMatch(st -> st.getStoreType().equals(StoreType.ACTIVE.name()));
+
+		this.address = site.getAddress1();
+		this.city = site.getCity();
+		this.state = site.getState();
+		this.postalCode = site.getPostalCode();
+		this.quad = site.getQuad();
+		this.intersectionStreetPrimary = site.getIntersectionStreetPrimary();
+		this.intersectionStreetSecondary = site.getIntersectionStreetSecondary();
 	}
 
 	public Integer getId() {
@@ -115,5 +134,69 @@ public class SiteMarker {
 
 	public void setUpdatedDate(LocalDateTime updatedDate) {
 		this.updatedDate = updatedDate;
+	}
+
+	public boolean isVacant() {
+		return vacant;
+	}
+
+	public void setVacant(boolean vacant) {
+		this.vacant = vacant;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getQuad() {
+		return quad;
+	}
+
+	public void setQuad(String quad) {
+		this.quad = quad;
+	}
+
+	public String getIntersectionStreetPrimary() {
+		return intersectionStreetPrimary;
+	}
+
+	public void setIntersectionStreetPrimary(String intersectionStreetPrimary) {
+		this.intersectionStreetPrimary = intersectionStreetPrimary;
+	}
+
+	public String getIntersectionStreetSecondary() {
+		return intersectionStreetSecondary;
+	}
+
+	public void setIntersectionStreetSecondary(String intersectionStreetSecondary) {
+		this.intersectionStreetSecondary = intersectionStreetSecondary;
 	}
 }
--- web/src/main/java/com/mtn/controller/StoreListController.java
+++ web/src/main/java/com/mtn/controller/StoreListController.java
@@ -1,60 +1,83 @@
 package com.mtn.controller;
 
-import com.mtn.model.domain.*;
-import com.mtn.model.simpleView.*;
-import com.mtn.model.view.*;
-import com.mtn.service.*;
+import com.mtn.constant.StoreListSearchType;
+import com.mtn.model.domain.AuditingEntity;
+import com.mtn.model.domain.Site;
+import com.mtn.model.domain.StoreList;
+import com.mtn.model.simpleView.SimpleStoreListView;
+import com.mtn.model.simpleView.SiteMarker;
+import com.mtn.model.simpleView.StoreMarker;
+import com.mtn.model.view.StoreListView;
+import com.mtn.service.SiteService;
+import com.mtn.service.StoreListService;
+import com.mtn.service.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
-
-import com.mtn.constant.StoreListSearchType;
 
 @RestController
 @RequestMapping("/api/store-list")
 public class StoreListController extends CrudController<StoreList, StoreListView> {
 
-	private final StoreService storeService;
+	private final SiteService siteService;
 
 	@Autowired
-	public StoreListController(StoreListService storeListService, StoreService storeService) {
+	public StoreListController(StoreListService storeListService,
+							   StoreService storeService,
+							   SiteService siteService) {
 		super(storeListService, StoreListView::new);
-		this.storeService = storeService;
+		this.siteService = siteService;
 	}
 
 	@GetMapping
 	public ResponseEntity findAll(Pageable page,
-			@RequestParam(value = "created-by-id", required = false) Integer createdById,
-			@RequestParam(value = "subscriber-ids", required = false) List<Integer> subscriberIds,
-			@RequestParam(value = "including-store-ids", required = false) List<Integer> includingStoreIds,
-			@RequestParam(value = "excluding-store-ids", required = false) List<Integer> excludingStoreIds,
-			@RequestParam(value = "search-type", required = false, defaultValue = "ANY") StoreListSearchType searchType) {
-		Page<StoreList> domainModels;
-		if (subscriberIds != null || includingStoreIds != null || excludingStoreIds != null) {
-			domainModels = ((StoreListService) this.entityService).findAllByQueryUsingSpecs(page, createdById,
-					subscriberIds, includingStoreIds, excludingStoreIds, searchType);
-		} else {
-			domainModels = this.entityService.findAllUsingSpecs(page);
-		}
+								  @RequestParam(value = "including-store-ids", required = false) List<Integer> includingStoreIds,
+								  @RequestParam(value = "excluding-store-ids", required = false) List<Integer> excludingStoreIds,
+								  @RequestParam(value = "search-type", required = false, defaultValue = "ANY") StoreListSearchType searchType) {
+		Page<StoreList> domainModels = ((StoreListService) this.entityService).findAllByQueryUsingSpecs(page, includingStoreIds, excludingStoreIds, searchType);
 		return ResponseEntity.ok(domainModels.map(SimpleStoreListView::new));
+	}
+
+	@GetMapping("/{id}/marker-data")
+	public ResponseEntity<List<SiteMarker>> getSiteMarkersForStoreList(@PathVariable("id") Integer storeListId) {
+		StoreList storeList = this.entityService.findOne(storeListId);
+		if (storeList.getStores().size() == 0) {
+			return ResponseEntity.ok(new ArrayList<>());
+		} else {
+			// Get the Store Ids that need to be included
+			Set<Integer> storeIds = storeList.getStores().stream().map(AuditingEntity::getId).collect(Collectors.toSet());
+			// Get the distinct set of Sites that need to be queried
+			List<Integer> siteIds = storeList.getStores().stream().map(st -> st.getSite().getId()).distinct().collect(Collectors.toList());
+			List<Site> sites = this.siteService.findAllByIdsUsingSpecs(siteIds);
+			List<SiteMarker> siteMarkers = sites.stream().map(si -> {
+				SiteMarker sm = new SiteMarker(si);
+				// Filter out stores that aren't in the specified list
+				List<StoreMarker> filteredStores = sm.getStores().stream().filter(st -> storeIds.contains(st.getId())).collect(Collectors.toList());
+				sm.setStores(filteredStores);
+				return sm;
+			}).collect(Collectors.toList());
+			return ResponseEntity.ok(siteMarkers);
+		}
 	}
 
 	@PutMapping(path = "/{id}/add-stores")
 	public ResponseEntity addStoresToStoreList(@PathVariable("id") Integer storeListId,
-			@RequestBody List<Integer> request) {
+											   @RequestBody List<Integer> request) {
 		StoreList storeList = ((StoreListService) this.entityService).addStoresToStoreList(storeListId, request);
-		return ResponseEntity.ok(new SimpleStoreListView(storeList));
+		return ResponseEntity.ok(new StoreListView(storeList));
 	}
 
 	@PutMapping(path = "/{id}/remove-stores")
 	public ResponseEntity removeStoresFromStoreList(@PathVariable("id") Integer storeListId,
-			@RequestBody List<Integer> request) {
+													@RequestBody List<Integer> request) {
 		StoreList storeList = ((StoreListService) this.entityService).removeStoresFromStoreList(storeListId, request);
-		return ResponseEntity.ok(new SimpleStoreListView(storeList));
+		return ResponseEntity.ok(new StoreListView(storeList));
 	}
 }

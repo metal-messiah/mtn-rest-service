46d3b3c9465068e7491f7e47131122dcaf68b8b6
Tyler Tippetts <tyjoti89@gmail.com>
2019-08-16 14:39:55 -0600

    Removed unused method

--- web/src/main/java/com/mtn/repository/specification/StoreSpecifications.java
+++ web/src/main/java/com/mtn/repository/specification/StoreSpecifications.java
@@ -1,6 +1,5 @@
 package com.mtn.repository.specification;
 
-import com.mtn.constant.StoreType;
 import com.mtn.model.domain.Site_;
 import com.mtn.model.domain.Store;
 import com.mtn.model.domain.Store_;
@@ -11,14 +10,5 @@ public class StoreSpecifications extends AuditingEntitySpecifications {
     public static Specification<Store> siteIdEquals(Integer id) {
         return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.equal(root.join(Store_.site).get(Site_.id), id);
     }
-
-    public static Specification<Store> storeTypeEquals(StoreType storeType) {
-        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.equal(root.get(Store_.storeType), storeType);
-    }
-
-    public static Specification<Store> isFloat() {
-        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.isTrue(root.get(Store_.floating));
-    }
-
 
 }

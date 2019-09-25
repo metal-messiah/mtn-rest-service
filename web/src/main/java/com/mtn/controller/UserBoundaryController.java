f12850eecaf723f2cf9d46064d6b5e178afa5ce2
Tyler Tippetts <tyjoti89@gmail.com>
2019-09-25 17:20:57 -0600

    Got Tuples working

--- web/src/main/java/com/mtn/controller/UserBoundaryController.java
+++ web/src/main/java/com/mtn/controller/UserBoundaryController.java
@@ -2,6 +2,7 @@ package com.mtn.controller;
 
 import com.mtn.model.domain.UserBoundary;
 import com.mtn.model.simpleView.SimpleUserBoundaryView;
+import com.mtn.model.utils.TupleUtil;
 import com.mtn.model.view.UserBoundaryView;
 import com.mtn.service.UserBoundaryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/user-boundary")
@@ -29,18 +33,21 @@ public class UserBoundaryController extends CrudController<UserBoundary, UserBou
 
 	@GetMapping
 	public ResponseEntity findAllForUser(@RequestParam("user-id") Integer userId) {
-		Query query = this.entityManager.createNamedQuery("getAllUserBoundariesForUser")
-				.setParameter("userProfileId", userId);
-
-//		Query query = this.entityManager.createNativeQuery("SELECT ub.user_boundary_id, ub.boundary_id, ub.boundary_name " +
-//				"from user_boundary ub " +
-//				"where ub.user_profile_id = :userProfileId " +
-//				"and ub.deleted_date is null", Tuple.class)
+//		Query query = this.entityManager.createNamedQuery("getAllUserBoundariesForUser")
 //				.setParameter("userProfileId", userId);
 
-		List<Tuple> resultList = query.getResultList();
+		List<Tuple> resultList = this.entityManager.createNativeQuery("SELECT ub.user_boundary_id as id, " +
+				"ub.boundary_id as boundaryId, " +
+				"ub.boundary_name as boundaryName " +
+				"from user_boundary ub " +
+				"where ub.user_profile_id = :userProfileId " +
+				"and ub.deleted_date is null", Tuple.class)
+				.setParameter("userProfileId", userId)
+				.getResultList();
 
-		return ResponseEntity.ok(resultList);
+
+
+		return ResponseEntity.ok(TupleUtil.toListOfMaps(resultList));
 	}
 
 }
